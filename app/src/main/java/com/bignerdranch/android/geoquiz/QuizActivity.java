06574f96aed6b2e7e9e81877c490f9eb80432158
Bellkross <vasilenkowm@yandex.ua>
2017-12-03 21:00:08 +0200

    add toast in top

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -27,9 +28,11 @@ public class QuizActivity extends AppCompatActivity {
         });
         */
         mFalseButton.setOnClickListener(v -> {
-            Toast.makeText(QuizActivity.this,
+            Toast toast = Toast.makeText(QuizActivity.this,
                     R.string.correct_toast,
-                    Toast.LENGTH_SHORT).show();
+                    Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.TOP,0,0);
+            toast.show();
         });
         mTrueButton.setOnClickListener(v -> {
             Toast.makeText(QuizActivity.this,
